cf271bf61a6f8ef4b2d755dd50f6d3c3cbc76fe1
osama-alshafie <osama809089@gmail.com>
2018-05-28 16:07:14 +0200

    add category controller

--- src/main/java/com/jwt/controller/CategoryController.java
+++ src/main/java/com/jwt/controller/CategoryController.java
@@ -1,12 +1,24 @@
 package com.jwt.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.jwt.service.CategoryService;
 
 @Controller
 @RequestMapping(value = "/category")
 public class CategoryController {
-
 	
+	@Autowired
+	private CategoryService CategoryService;
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public String getCategory(Model model, @PathVariable("id") int id) {
+		model.addAttribute("category", CategoryService.getCategoryById(id)) ;
+		return "category";
+	}
 	
 }
