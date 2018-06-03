b8d51367b026b1c6c8944af9136187d50b7e9f7c
hisham-azmy <hisham-azmy>
2018-06-03 13:14:53 +0200

    image

--- src/main/java/com/jwt/controller/ProductController.java
+++ src/main/java/com/jwt/controller/ProductController.java
@@ -5,9 +5,6 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
